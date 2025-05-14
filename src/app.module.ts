2da5b3d3bf717628693efc7cc6f679d9a35a9e9e
MascielFuentesSales <masciel.fuentes@gmail.com>
2025-05-13 19:44:02 -0600

    libros

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,14 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { TicketsController } from './tickets/tickets.controller';
+import { TicketsService } from './tickets/tickets.service';
+import { LibrosController } from './libros/libros.controller';
+import { LibrosService } from './libros/libros.service';
 
 @Module({
   imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  controllers: [AppController, TicketsController, LibrosController],
+  providers: [AppService, TicketsService, LibrosService],
 })
 export class AppModule {}
